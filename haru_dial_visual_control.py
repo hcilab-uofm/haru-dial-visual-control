c2c87674802c3ae1de779dd4c9d6dbff2ffad8a6
Ahmed Shariff <shariff.mfa@outlook.com>
2022-12-06 17:44:49 -0800

    Adding missed include & pyinstaller scripts

---
+++ haru_dial_visual_control.py
@@ -0,0 +1,9 @@
+from haru_dial_visual_control._expression_ui import haru_expression_gui
+
+
+def main():
+    haru_expression_gui()
+
+
+if __name__ == '__main__':
+   main()
---
+++ pyinstall.bat
@@ -0,0 +1,1 @@
+poetry run pyinstaller .\haru_dial_visual_control.py -F --hidden-import haru_dial_visual_control -p C:\Users\amsha\AppData\Local\pypoetry\Cache\virtualenvs\haru-dial-visual-control-nhfrElyr-py3.10 -c --copy-metadata haru_dial_visual_control --add-data haru_dial_visual_control/static;haru_dial_visual_control/static
